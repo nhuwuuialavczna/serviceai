1091ea5c04ddaf3f85658ec264d42191cb4755ed
vegito165 <nguyentanhau1651997@gmail.com>
2017-11-13 21:07:59 +0700

    z

--- app.js
+++ app.js
@@ -4,11 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var index = require('./routes/index');
+
+
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// url for HomeController
 app.use('/', index);
+
 module.exports = app;
 app.listen(process.env.PORT || 3000);
