e564a642a90ce0890ed3149c078a175066488f70
vegito165 <nguyentanhau1651997@gmail.com>
2017-11-13 20:52:06 +0700

    z

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
+module.exports = app;
+app.listen(process.env.PORT || 3000);
