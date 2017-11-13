64ad9d18ccb5412f3d9e6ce6bb33cceb1f173d20
vegito165 <nguyentanhau1651997@gmail.com>
2017-11-13 22:02:01 +0700

    z

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var us = function (name, pass) {
+    this.name = name;
+    this.pass = pass;
+};
+var userRegister = [];
+var hau = new us("hau", "145632");
+var tan = new us("tan", "123482");
+
+
+userRegister.push(hau);
+userRegister.push(tan);
+
+function equals(us) {
+    for (var i = 0; i < userRegister.length; i++) {
+        var t = userRegister[i];
+        if (t.name === us.name && t.pass === us.pass) {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var name = req.param('name');
+    var code = req.param('code');
+
+    var usss = new us(name, code);
+    console.log(equals(usss));
+    if (equals(usss)) {
+        res.json({"result": "success"});
+    } else {
+        res.json({"result": "fail"});
+    }
+
 });
 
 module.exports = router;
