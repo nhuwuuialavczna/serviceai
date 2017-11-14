4b26a8bba1c5604d12f4c49aa6e94777b51b9e00
vegito165 <nguyentanhau1651997@gmail.com>
2017-11-14 09:59:09 +0700

    z

--- routes/index.js
+++ routes/index.js
@@ -16,10 +16,10 @@ var Admin = function Admin(code) {
 // users
 var userRegister = [];
 var hau = new Users("RD0003FF442501", "42.119.222.181", "SG", 10.8142, 106.6438);
-var canh = new Users("RD0003FF442501", "42.119.222.181", "SG", 10.8142, 106.6438);
+var canh = new Users("RD0003FF442501", "119.17.248.18", "SG", 21.0333, 105.85);
 
 userRegister.push(hau);
-
+userRegister.push(canh);
 // admin
 var adminRegister = [];
 var admin = new Admin("165997");
