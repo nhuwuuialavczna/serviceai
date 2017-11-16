c957ea5bf7d06b98effe890958ea1cf7d3823003
vegito165 <nguyentanhau1651997@gmail.com>
2017-11-16 10:02:32 +0700

    z

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var url = require('url');
 var os = require("os");
-
+var sql = require("mssql");
+var config = {
+    user: 'chat@chatser',
+    password: 'Abcdabcd1',
+    server: 'chatser.database.windows.net',
+    database: 'chatserver',
+    options: { encrypt: 'true', database: 'sensorData'}
+};
 var Users = function Users(name, ip, region_code, latitude, longitude,information) {
     this.name = name;
     this.ip = ip;
@@ -12,55 +19,50 @@ var Users = function Users(name, ip, region_code, latitude, longitude,informatio
     this.longitude = longitude;
     this.information = information;
 };
-var Admin = function Admin(code) {
-    this.code = code;
-};
+
 
 function containUser(us,userRegister) {
-    for (var i = 0; i < userRegister.length; i++) {
-        var t = userRegister[i];
-        if (t.ip === us.ip && t.region_code === us.region_code && t.latitude === us.latitude && t.longitude === us.longitude) {
-            return true;
-        }
-    }
+
     return false;
 }
 
 router.get('/', function (req, res) {
     var info = req.param('info');
+    var timeLogin = req.param('timelogin');
     var ip = JSON.parse(info);
     var hostaddress = os.hostname();
     var obj = new Users(hostaddress,ip.ip,ip.region_code,ip.latitude,ip.longitude);
-    // config for your database
-    var sql = require("mssql");
-    var config = {
-        user: 'chat@chatser',
-        password: 'Abcdabcd1',
-        server: 'chatser.database.windows.net',
-        database: 'chatserver',
-        options: { encrypt: 'true', database: 'sensorData'}
-    };
-
     sql.connect(config, function (err) {
         if (err) console.log(err);
         var request = new sql.Request();
         request.query('select * from UsersTable', function (err, recordset) {
             if (err) console.log(err);
             var userRegister = recordset.recordsets[0];
-            if(containUser(obj,userRegister)){
-                res.json({re: 'success'});
-            } else {
-                res.json({re: 'fail'});
+            for (var i = 0; i < userRegister.length; i++) {
+                var t = userRegister[i];
+                if (t.ip === us.ip && t.region_code === us.region_code && t.latitude === us.latitude && t.longitude === us.longitude) {
+                    themVaoBangDangNhap(t.name,t.ip,timeLogin);
+                    res.json({re: 'success'});
+                }else{
+                    res.json({re: 'fail'});
+                }
             }
             sql.close();
         });
     });
 });
-
+var themVaoBangDangNhap = function (name,ip,time) {
+    sql.connect(config, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.query("insert into TimeLogin values('"+name+"','"+ip+"','"+time+"')", function (err, recordset) {
+            if (err) console.log(err);
+        });
+    });
+};
 
 router.get('/admin', function (req, res, next) {
     var code = req.param('code');
-
     if (code === '147258') {
         res.json({re: 'success'});
     } else {
@@ -68,5 +70,11 @@ router.get('/admin', function (req, res, next) {
     }
 });
 
+router.get('/login', function (req, res, next) {
+    var infologin = req.param('infologin');
+    var us = JSON.parse(infologin);
+    themVaoBangDangNhap(us);
+    res.json({re:'success'});
+});
 
 module.exports = router;
